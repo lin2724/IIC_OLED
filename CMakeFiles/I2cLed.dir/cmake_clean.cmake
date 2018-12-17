46389dc1fe656c1b5bd560c373764be33420cc31
zzl <lin2724@msn.com>
2018-12-17 20:24:11 +0800

    new file:   CMakeLists.txt 	renamed:    src/log.h -> inc/log.h 	deleted:    src/main.cpp

---
+++ CMakeFiles/I2cLed.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/I2cLed.dir/src/led_i2c_command.cpp.o"
+  "CMakeFiles/I2cLed.dir/src/log.cpp.o"
+  "I2cLed.pdb"
+  "I2cLed"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/I2cLed.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
